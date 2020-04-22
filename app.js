5a6c2e09c9a85c107d3163db222d8edae9870140
Gia Edgington <gia.edgington@gmail.com>
2020-04-22 11:40:27 -0400

    Create books route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const bookRoutes = require('./routes/books');
+
+app.use('/', bookRoutes);
+
 
 app.get('/', (req, res) => {
     res.send('Before You Go');
