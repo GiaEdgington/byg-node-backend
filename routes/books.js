8bffd05375335b6122cedc8738d641c6605a07fd
Gia Edgington <gia.edgington@gmail.com>
2020-04-22 12:15:01 -0400

    Create books controller / getBooks function

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,8 @@
 const express = require('express');
 
+const booksController = require('../controllers/books');
 const router = express.Router();
 
-router.get('/books', (req, res) => {
-    res.send('this is for books');
-});
+router.get('/books', booksController.getBooks);
 
 module.exports = router;
