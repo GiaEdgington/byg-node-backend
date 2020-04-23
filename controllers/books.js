085bf6dd1d4de240ee8472993491da678b0457b4
Gia Edgington <gia.edgington@gmail.com>
2020-04-23 14:10:44 -0400

    Format data result

--- controllers/books.js
+++ controllers/books.js
@@ -8,7 +8,8 @@ exports.getBooks = async (req, res) => {
     var bookList = [];
 
     try{
-        const response = await fetch(encodeURI(`https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=Category:${destination}&cmlimit=18&origin=*`));
+        //Fetch Wikipedia API for books destination based --- LIMIT SET TO 5
+        const response = await fetch(encodeURI(`https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=Category:${destination}&cmlimit=5&origin=*`));
 
         const bookTitles = await response.json();
 
@@ -16,15 +17,23 @@ exports.getBooks = async (req, res) => {
             books.push(title.title);
         });
  
+        //Need to await Promise.all for aditional async fetch
         await Promise.all(books.map(async (book) => {
             let result = await fetch(`https://www.googleapis.com/books/v1/volumes?q=+title:${book}&maxResults=1&key=${GOOGLE_BOOKS_API_KEY}`);
             let resJson = await result.json();
-            //console.log(resJson);
-            bookList.push(resJson);
-            })
+            
+            let title = resJson.items[0].volumeInfo.title === "undefined" ? "" : resJson.items[0].volumeInfo.title;
+            let authors = resJson.items[0].volumeInfo.authors === "undefined" ? "" : resJson.items[0].volumeInfo.authors;
+            let image = resJson.items[0].volumeInfo.imageLinks === "undefined" ? "" : resJson.items[0].volumeInfo.imageLinks.smallThumbnail;
+            let synopsis = resJson.items[0].volumeInfo.description === "undefined" ? "Information not available." : resJson.items[0].volumeInfo.description;
+
+            let bookData = [ title, authors, image, synopsis ];
+
+            bookList.push(bookData);
+            //console.log(resJson.items[0].volumeInfo.authors);
+            }) 
         );
-        console.log(bookList[0]);
-        //res.send(books);
+        res.send(bookList);
     } catch (err) {
         console.log(err);
     }
