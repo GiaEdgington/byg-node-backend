56d3aa2571bb7c4dfa612d76690b30c8c5dbafd0
Gia Edgington <gia.edgington@gmail.com>
2020-05-01 11:44:55 -0400

    Create destination route and controller

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const booksController = require('../controllers/books');
+
 const router = express.Router();
 
 router.get('/books', booksController.getBooks);
 
 router.post('/book', booksController.saveBook);
+
+router.post('/destination', booksController.addDestination);
 
 module.exports = router;
