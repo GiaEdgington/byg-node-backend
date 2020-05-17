052e6cc2af4ef288ad8bf6497443df7659b67ce8
Gia Edgington <gia.edgington@gmail.com>
2020-05-17 13:41:29 -0400

    Create User schema

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    destinations: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Destination'
+        }
+    ]
+
+})
+
+module.exports = mongoose.model('User', userSchema);
