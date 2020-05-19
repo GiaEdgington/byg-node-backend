06dabf6038dc2b7be7cc6f93463be5508762e58e
Gia Edgington <gia.edgington@gmail.com>
2020-05-19 18:36:27 -0400

    Create users route/ get all controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,18 @@ let authController = {
         let newUser = new User(req.body);
         let result = await newUser.save();
         res.json({ message: 'User created!', userId: result._id });
+    },
+    getUsers: async (req, res) => {
+        try{
+            const users = await User.find();
+            res.status(200)
+            .json({
+                message: 'All users.',
+                users: users
+            });
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
-const User = require('../models/user');
+
 const authController = require('../controllers/auth');
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
+router.post('/users', authController.signup);
+
+router.get('/users', authController.getUsers);
+
+module.exports = router;
